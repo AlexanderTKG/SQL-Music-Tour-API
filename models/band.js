a53436e76c9fd2f8aed55c56f1a39d12d3b979d2
Alexander Martinez <Alexander-MD@outlook.com>
2022-10-19 19:03:18 -0400

    Calling init method on the Band class

---
+++ models/band.js
@@ -0,0 +1,38 @@
+// DEPENDENCIES
+const { Sequelize, Database, Model, DataTypes } = require('sequelize')
+const sequelize = new Sequelize(process.env.PG_URI)
+
+//Model
+class Band extends Model{}
+
+Band.init({
+    band_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    Name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    genre: {
+        type: DataTypes.TEXT,
+        allowNull: false
+    },
+    available_start_time: {
+        type: DataTypes.DATE,
+        allowNull: false
+    },
+    end_time: {
+        type: DataTypes.DATE,
+        allowNull: false
+    },
+}, {
+    sequelize,
+    modelName: 'Band',
+    tableName: 'band',
+    timestamps: false,
+})
+
+// EXPORT
+module.exports = Band
