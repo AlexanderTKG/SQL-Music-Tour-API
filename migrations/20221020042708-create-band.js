a16c8c6027f3512c6d9ce68d619abddc0e7aad5b
Alexander Martinez <Alexander-MD@outlook.com>
2022-10-20 00:36:04 -0400

    The Migration file

--- migrations/20221020042708-create-band.js
+++ migrations/20221020042708-create-band.js
@@ -3,38 +3,31 @@
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('Bands', {
-      id: {
+      band_id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      band_id: {
-        type: Sequelize.INTEGER
-      },
       name: {
-        type: Sequelize.STRING
-      },
-      genre: {
-        type: Sequelize.TEXT
+        type: Sequelize.STRING,
+        allowNull: false
       },
       available_start_time: {
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
+        allowNull: false
       },
       end_time: {
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
+        allowNull: false
       },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
       },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
     });
   },
-  async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Bands');
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('bands');
   }
 };
